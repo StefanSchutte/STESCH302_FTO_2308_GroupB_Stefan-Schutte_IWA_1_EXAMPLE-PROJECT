14c40cc21257b3c9f0a120f663ff66f85bc415ee
Stefan Schutte <2011070287@ufs4life.ac.za>
2023-10-19 11:28:04 +0200

    addedlect4

--- scripts.js
+++ scripts.js
@@ -1,3 +1,84 @@
+//import { hello as helloA } from './code-a.js';
+//import { hello as helloB } from './code-b.js';
+
+
+/*
+//------------------------Variables and Lexical Scope---------------------------------
+const valueA = 123;
+const valueB = 10;
+const valueC = valueA + valueB;
+const valueD = valueB;
+
+console.log(typeof valueA);
+console.log(valueA, valueA.toString());
+
+let example = 20;
+
+const test1 = () => {
+const value = 1
+    console.log(value)
+}
+const test2 = () => {
+const value = 2;
+    console.log(value)
+}
+
+//closures - declare var in parent scope - access available
+{
+const value = 5
+const test3 = () => {
+
+    console.log(value)
+}
+const test4 = () => {
+
+    console.log(value)
+}
+test3();
+test4();
+}
+
+
+const valueZ = 10;
+
+if (true){
+    console.log(valueZ);
+
+    if (true){
+        console.log(valueZ)
+
+        if (false){
+            console.log(valueZ);
+        }
+    }
+}
+
+const exampleA = 10;
+
+//constant cant be reassigned. let can
+
+let exampleB = 20;
+console.log(exampleB);
+exampleB = 2;
+console.log(exampleB);
+
+var exampleC = 30;//dont use
+
+if (true) {
+    //const exampleB = 8;
+    //console.log(exampleB);
+    if (true){
+        const exampleB = 3;
+        console.log(exampleB)
+    }
+}
+
+*/
+
+//--------------------------CommentOut & Console.Log------------------------------------------------
+
+//console.log(helloA, helloB)
+
 // @ts-check
 
 // const id = 12345
@@ -20,8 +101,11 @@
 
 //throw new Error('something went wrong')
 
+
+
 //console.log('helo world');
 
+//global consts
 const MAX_NUMBER = 15;
 const MIN_NUMBER = -5;
 const STEP_AMOUNT = 5;
@@ -37,30 +121,32 @@ const subtractHandler = () => {
     const newValue = parseInt(number.value ) - STEP_AMOUNT;
     number.value = newValue;
 
-    if (add.disabled === true){
+    if (add.disabled ){
         add.disabled = false;
     }
 
     if (newValue <= MIN_NUMBER){
         subtract.disabled = true;
-    }else{
-        subtract.disabled = false;
     }
+    //else{
+    //    subtract.disabled = false;
+    //}
 }
 const addHandler = () => {
     console.log('add was clicked');
     const newValue = parseInt(number.value) + STEP_AMOUNT;
     number.value = newValue;
 
-    if (subtract.disabled === true){
+    if (subtract.disabled){
         subtract.disabled = false;
     }
 
     if (newValue >= MAX_NUMBER){
         add.disabled = true;
-    }else{
-        add.disabled = false;
     }
+    //else{
+    //    add.disabled = false;
+    //}
 }
 
 // parseInt('123')
